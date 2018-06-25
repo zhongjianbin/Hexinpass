59cfc31cd90fc6607fe82e523f919f2a6a02575b
zhongjianbin <iszhongjianbin@163.com>
2018-06-25 11:19:50 +0800

    add suggestion page case

---
+++ app/src/androidTest/java/projects/wlye/cases/subcases/submycases/SuggestionCase.java
@@ -0,0 +1,12 @@
+package projects.wlye.cases.subcases.submycases;
+
+import helper.base.VP2;
+
+/**
+ * Author:jianbin.zhong
+ * Date: 2018/6/25 11:19
+ * Description:
+ */
+
+public class SuggestionCase extends VP2 {
+}
